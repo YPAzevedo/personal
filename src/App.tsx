9c41d03b7b49d042d20a8f8c065838d8de296b94
YPAzevedo <yago.pereira.az@gmail.com>
2025-10-19 21:41:11 -0700

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,22 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import "./index.css";
-
-import { Textarea } from "./components/ui/textarea";
-import logo from "./logo.svg";
-import reactLogo from "./react.svg";
 
 export function App() {
   return (
-    <div className="container mx-auto p-8 text-center relative z-10">
-      <div className="flex justify-center items-center gap-8 mb-8">
-        <img
-          src={logo}
-          alt="Bun Logo"
-          className="h-36 p-6 transition-all duration-300 hover:drop-shadow-[0_0_2em_#646cffaa] scale-120"
-        />
-        <img
-          src={reactLogo}
-          alt="React Logo"
-          className="h-36 p-6 transition-all duration-300 hover:drop-shadow-[0_0_2em_#61dafbaa] [animation:spin_20s_linear_infinite]"
-        />
+    <div className="flex flex-col items-center justify-center w-screen min-h-screen bg-black text-green-400 font-mono p-4">
+      <div className="border-2 border-dashed border-green-400 p-6 mb-6">
+        <pre className="text-center text-sm mb-4">
+          {`
+  ███████  ██████  █████ ████  ██████    ███████  ██████ 
+ ███░░███ ███░░███░░███ ░███  ░░░░░███  ███░░███ ███░░███
+░███ ░███░███ ░███ ░███ ░███   ███████ ░███ ░███░███ ░███
+░███ ░███░███ ░███ ░███ ░███  ███░░███ ░███ ░███░███ ░███
+░░███████░░██████  ░░███████ ░░████████░░███████░░██████ 
+ ░░░░░███ ░░░░░░    ░░░░░███  ░░░░░░░░  ░░░░░███ ░░░░░░  
+ ███ ░███           ███ ░███            ███ ░███         
+░░██████           ░░██████            ░░██████          
+ ░░░░░░             ░░░░░░              ░░░░░░           `}
+        </pre>
       </div>
-      <Card>
-        <CardHeader className="gap-4">
-          <CardTitle className="text-3xl font-bold">Bun + React</CardTitle>
-          <CardDescription>
-            Edit{" "}
-            <code className="rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono">
-              src/App.tsx
-            </code>{" "}
-            and save to test HMR
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Textarea />
-        </CardContent>
-      </Card>
     </div>
   );
 }
-
-export default App;
