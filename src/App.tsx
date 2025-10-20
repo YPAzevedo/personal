c8f3faa9933d5fdb9651aca6ff8dc34ac12a3ed9
YPAzevedo <yago.pereira.az@gmail.com>
2025-10-19 21:59:38 -0700

    fix mobile size

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import "./index.css";
 export function App() {
   return (
     <div className="flex flex-col items-center justify-center w-screen min-h-screen bg-black text-green-400 font-mono p-4">
-      <div className="border-2 border-dashed border-green-400 p-6 mb-6">
-        <pre className="text-center text-sm mb-4">
+      <div className=" border-2 border-dashed border-green-400 p-6 mb-6">
+        <pre className="text-center text-[8px] sm:text-base transition-[font-size] duration-300 ease-in-out mb-4">
           {`
   ███████  ██████  █████ ████  ██████    ███████  ██████ 
  ███░░███ ███░░███░░███ ░███  ░░░░░███  ███░░███ ███░░███
